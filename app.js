785a7b653ca37bb1989de063b09d5b0938c9c1c8
Aeronnax <aeronnaxog@gmail.com>
2022-12-07 02:26:55 +0600

    МАССИВ

--- app.js
+++ app.js
@@ -1,81 +1,112 @@
-// //1 Функции 
-// //Function Declaration  можно обращатся когда хотим 
-// function greet(name){
-//     console.log ('Привет - ', name)
+// const myGuild = ['Лианоара', 'Кадзу', 'Грэйк','Беовульф']  //Можно хранить строки
+// const people = [
+//     {name: 'Дима', budget: 5000},
+//     {name: 'Илья', budget: 4500},
+//     {name: 'Кирилл', budget: 4000},
+// ]
+// const fib = [1, 1, 2, 3, 5, 8, 13 ] //Числа и булевувые значения  
+
+//console.log(myGuild)  //Массив это объект, принял понял. 
+
+// function addItemToEnd(){  //функция
+
 // }
 
-// //Function Expression   Создаем функцию и складываем ее в переменную. Нельзя получить доступ до инициализации
-// const greet2 = function greet2(name){     
-//     console.log ('Привет 2 - ', name)
-// }
+//Method
+// myGuild.push('Джининг')  //положил Лешу в конец
+// myGuild.unshift('Фливи') //положил Илюху в начало 
 
-//  greet('Дима')
-// greet2('Дима')
-// //Function Expression   будет ошибка   Cannot access 'greet2' before initialization
-// // const greet2 = function greet2(name){     
-//      console.log ('Привет 2 - ', name)
-//  }
+//myGuild.shift() //можно удалить первый элемент с массива
+//myGuild.pop()  //можно удалить последний элемент с массива
 
-//  console.log(typeof greet) скажет что это функция. А по факту объект 
-//  console.dir(greet) 
+// const myGuildClean = myGuild.shift()  // Удаляешь, но все еще можешь с ним работать 
+// const myGuildCleanend = myGuild.pop()
+// console.log(myGuildClean)
+// console.log(myGuildCleanend)
+// console.log(myGuild)
 
-// 2 Анонимные функции
-// let counter = 0
-// const interval = setInterval(function(){  //setTimeout
-//     if (counter === 5) {
-//         clearInterval(interval)  //clearTimeout
-//     } else {
-//       console.log(++counter)
-//     }
-// },1000)
+// console.log(myGuild.reverse())   //Название говорит за себя
+// console.log(myGuild)
 
-// 3 Стрелочные функции   Поясни за такой тип функций 
+// const index = myGuild.indexOf('Лианоара')  //хорошо работает с примитивными данными 
+// myGuild[index] = 'Это всего лишь игра'  // Изменили отдельно взятый индекс
+// console.log(myGuild)
 
-// function greet(name){
-//     console.log ('Привет - ', name)
-// }
+// const index = people.findIndex(function(person){ // таким образом можно узнать каждый объект находящийся в массиве
+//     console.log(person) 
+// }) 
+// const index = people.findIndex(function(person){
+//     return person.budget === 4500
+// }) 
+// const person = people.find(function(person){   // результат один и тот же. Подход разный 
+//     return person.budget === 4500
 
-// const arrow = (name) => {
-//     console.log('Привет - ', name)
-// }
-// arrow('Дима')
+// }) 
+// console.log(person)
 
-// const arrow2 = name => console.log('Привет - ', name)
-
-// arrow2('Илюша')
-
-// // const pow2 = num => { // логика одна и таже 
-// //     return num ** 2 
-// // }
-// const pow2 = num => num ** 2 
-
-// console.log(pow2(5))
-
-
-
-//4 Параметры по умолчанию 
-// const sum = (a, b = 1) => a + b
-
-// console.log(sum(41,5))
-
-// function sumAll(...all){      //Весьма инетерсно что так можно "складывать" числа и не только
-// let result = 0
-// for (let num of all) {
-//     result += num
-// }
-// return result
-// }
-// const res = sumAll(1,2,3,4)
-// console.log(res)
-
-// 5 Замыкание   Поясни за логику в целом. Не совсем понимаю как работает. Мб надо просто переварить и снова вернуться к этому 
-// function createMember(name) {
-//     return function(lastName) {
-//         console.log(name + lastName)
+// let findedPerson        //выглядит грамоздко. 
+// for (const person of people){
+//     if (person.budget === 4000){   
+//         findedPerson = person
 //     }
 // }
+// console.log(findedPerson)
 
-// const logWithlastName =createMember ('Дима')
-// console.log(logWithlastName)
-// console.log(logWithlastName(' Лагода'))
-// console.log(logWithlastName(' Хуягода'))
+// const person = people.find((person)=>{    //можно еще упростить 
+//     return person.budget === 4000 
+// })
+// console.log(person)  // если нет значиние то будет ундивайнд
+
+// Лямда функция 
+// const person = people.find(person=>person.budget === 4000)  //Вспомнил мем Дмитрий/Дима/Димасик/ДОМЕСТОС
+// console.log(person)
+
+// console.log(myGuild.includes('Грэйк'))   //Спрашивает "Есть такой элемент в массиве"
+
+// const upperCaseGuild = myGuild.map(guild => {    // .map всегда возращает новый массив.  
+//     return guild.toUpperCase()
+// })
+// console.log(upperCaseGuild)
+// console.log(myGuild)
+
+
+// const  pow2 = num => num ** 2
+// const sqrt = num => Math.sqrt(num)
+
+// const pow2Fib = fib.map(pow2).map(Math.sqrt)  
+// console.log(pow2Fib)
+
+// const  pow2 = num => num ** 2
+// const pow2Fib = fib.map(pow2).map(Math.sqrt) 
+// const filteredNumbers = pow2Fib.filter(num =>{
+//     return num > 5
+// })
+// console.log(pow2Fib)
+// console.log(filteredNumbers)
+// Задача 1
+// const text = 'Привет, у нас 300 вайпов на нерзуле'
+// //const reverseText = text.split(',')   //Получаем массив из двух элементов
+// const reverseText = text.split('').reverse().join('')   //Получаем массив из каждой строчки
+// console.log(reverseText)
+
+const people = [
+    {name: 'Дима', budget: 5000},
+    {name: 'Илья', budget: 4500},
+    {name: 'Кирилл', budget: 4000},
+]
+// const allbudget = people.reduce((acc,person)=>{  //сделано немного не правильно
+//     if (person.budget > 4200){
+//         acc +=person.budget
+//     }
+    
+//     return acc
+
+// }, 0)
+
+// const allbudget = people    //Нужно твое мнение 
+// .filter(person => person.budget >4200)
+// .reduce((acc,person)=>{
+//     acc +=person.budget
+//     return acc
+// }, 0)
+// console.log(allbudget)
